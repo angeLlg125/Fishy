955fc31ba9ae15b13413a225bb72d4dcb9251988
unknown <angel.lg125@gmail.com>
2021-03-08 11:28:13 -0600

    delete testing text

--- src/Fishy/Fishy.java
+++ src/Fishy/Fishy.java
@@ -7,7 +7,7 @@ public class Fishy {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {sdgfdxg dfgfdg dh dfhdfh df hdf hdf 
+    public static void main(String[] args) {
         new GameBoard();
     }
 }
